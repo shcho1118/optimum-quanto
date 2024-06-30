27983fca6b40140a7f554bde014083de994b0239
shcho1118 <chosanghoon1118@gmail.com>
2024-06-30 21:36:23 +0900

    refactor: add comments for MarlinPackedTensor Weight permutation for Marlin is quite difficult to understand without explanation.

--- optimum/quanto/tensor/qbits/marlin/packed.py
+++ optimum/quanto/tensor/qbits/marlin/packed.py
@@ -10,22 +10,38 @@ __all__ = ["MarlinPackedTensor"]
 
 
 # From: https://github.com/IST-DASLab/marlin/blob/master/marlin/__init__.py#L40
+# this func does 2 things
+# 1. 1 thread can load 32 4bit == 128bit weights used for mulitple mma instructions at once
+# 2. faster dequant via parallel half2 mul
 def _get_perm():
     perm = []
+    # 32 == # of threads in 1 warp
     for i in range(32):
         perm1 = []
+        # column id in 16x8 weight block
+        # check https://docs.nvidia.com/cuda/parallel-thread-execution/#warp-level-matrix-fragment-mma-16816-float
         col = i // 4
+        # 1 32bit (int32) == 8 4bit, 1 thread has 4 weights per 16x8 & 4bit weights are packed in int32, so needs 2 16x8 == 1 16x16 blocks
         for block in [0, 1]:
+            # row id in 16x8 weight block
+            # check https://docs.nvidia.com/cuda/parallel-thread-execution/#warp-level-matrix-fragment-mma-16816-float
             for row in [
                 2 * (i % 4),
                 2 * (i % 4) + 1,
                 2 * (i % 4 + 4),
                 2 * (i % 4 + 4) + 1,
             ]:
+                # 8 weights used for 1 thread (16x16 block) are contiguous in memory via interleaving
+                # e.g. T0 uses (0, 16, 128, 144, 8, 24, 136, 152)
                 perm1.append(16 * row + col + 8 * block)
+        # 1 128bit (int4) == 4 32bit, 1 thread loads 128bit at once, so needs 4 16x16 == 1 16x64 blocks
         for j in range(4):
+            # 32 weights loaded by 1 thread (16x64 block) are contiguous in memory via interleaving
+            # e.g. T0 uses ((0 ~ 152) + 0 * 256, (0 ~ 152) + 1 * 256, ..., (0 ~ 152) + 3 * 256)
             perm.extend([p + 256 * j for p in perm1])
     perm = np.array(perm)
+    # for faster dequant
+    # check https://arxiv.org/pdf/2211.10017
     interleave = np.array([0, 2, 4, 6, 1, 3, 5, 7])
     perm = perm.reshape((-1, 8))[:, interleave].ravel()
     perm = torch.from_numpy(perm)
@@ -40,10 +56,12 @@ def pack(unpacked: torch.Tensor):
     w = unpacked
     N, K = w.shape
     w = unpacked.t()
+    # 16 == tile size, marlin uses 16x16 tile, so 16x16 grouping via interleaving 
     w = w.reshape((K // 16, 16, N // 16, 16))
     w = w.permute((0, 2, 1, 3))
     w = w.reshape((K // 16, N * 16))
     res = w
+    # _perm.numel() == 1024 == 4 16x16, permute weights with 4 16x16 unit for efficient mma + dequant
     res = res.reshape((-1, _perm.numel()))[:, _perm].reshape(res.shape)
     p = np.zeros((res.shape[0], res.shape[1] // 8), dtype=np.uint32)
     res = res.cpu().numpy().astype(np.uint32)
@@ -84,7 +102,6 @@ class MarlinPackedTensor(torch.Tensor):
 
     def __tensor_flatten__(self):
         inner_tensors = ["_data"]
-        # Since meta can be used for serialization, use only AST compatible strings
         meta = {
             "size": str(list(self.size())),
             "stride": str(self.stride()),
@@ -96,7 +113,6 @@ class MarlinPackedTensor(torch.Tensor):
         assert len(inner_tensors) == 1
         assert len(meta) == 2
         data = inner_tensors["_data"]
-        # Meta should contain only AST compatible strings
         size = ast.literal_eval(meta["size"])
         stride = ast.literal_eval(meta["stride"])
         return MarlinPackedTensor(data, size, stride)
@@ -105,7 +121,6 @@ class MarlinPackedTensor(torch.Tensor):
 
     @classmethod
     def __torch_dispatch__(cls, op, types, args, kwargs=None):
-        # Convert back to tensor before calling any operation except detach and move
         if op.overloadpacket is torch.ops.aten.detach:
             t = args[0]
             data = op(t._data)
@@ -116,7 +131,6 @@ class MarlinPackedTensor(torch.Tensor):
             if dtype != torch.uint8:
                 raise ValueError(f"MarlinPackedTensor are torch.uint8 only and cannot be moved to {dtype}.")
             device = kwargs.get("device", t.device)
-            # MarlinPackedTensor can only be moved to CUDA devices
             if device.type == "cuda":
                 data_kwargs = copy(kwargs)
                 data_kwargs["dtype"] = t._data.dtype
